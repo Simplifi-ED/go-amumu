4fa06f4ecc95b3f913114a1d5a3df90c58fb0547
Marouane Bouaricha <marouane@itchallenge.fr>
2024-01-29 12:04:43 +0100

    Added InitializeGraphForUserAuth function

--- graphhelper/graphhelper.go
+++ graphhelper/graphhelper.go
@@ -1,7 +1,13 @@
 package graphhelper
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
+	auth "github.com/microsoft/kiota-authentication-azure-go"
 	msgraphsdk "github.com/microsoftgraph/msgraph-sdk-go"
 )
 
@@ -14,4 +20,44 @@ type GraphHelper struct {
 func NewGraphHelper() *GraphHelper {
 	g := &GraphHelper{}
 	return g
+}
+
+func (g *GraphHelper) InitializeGraphForUserAuth() error {
+	clientId := os.Getenv("CLIENT_ID")
+	tenantId := os.Getenv("TENANT_ID")
+	scopes := os.Getenv("GRAPH_USER_SCOPES")
+	g.graphUserScopes = strings.Split(scopes, ",")
+
+	// Create the device code credential
+	credential, err := azidentity.NewDeviceCodeCredential(&azidentity.DeviceCodeCredentialOptions{
+		ClientID: clientId,
+		TenantID: tenantId,
+		UserPrompt: func(ctx context.Context, message azidentity.DeviceCodeMessage) error {
+			fmt.Println(message.Message)
+			return nil
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	g.deviceCodeCredential = credential
+
+	// Create an auth provider using the credential
+	authProvider, err := auth.NewAzureIdentityAuthenticationProviderWithScopes(credential, g.graphUserScopes)
+	if err != nil {
+		return err
+	}
+
+	// Create a request adapter using the auth provider
+	adapter, err := msgraphsdk.NewGraphRequestAdapter(authProvider)
+	if err != nil {
+		return err
+	}
+
+	// Create a Graph client using request adapter
+	client := msgraphsdk.NewGraphServiceClient(adapter)
+	g.userClient = client
+
+	return nil
 }
