a360533da02114e40ebdef06b9a6050d4e708483
Marouane Bouaricha <marouane@itchallenge.fr>
2024-01-29 12:02:39 +0100

    Added main entry point

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"go-send/graphhelper"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("Go Graph APP")
+	fmt.Println()
+
+	// Load .env files
+	// .env.local takes precedence (if present)
+	godotenv.Load(".env.local")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env")
+	}
+
+	graphHelper := graphhelper.NewGraphHelper()
+
+	initializeGraph(graphHelper)
+
+	greetUser(graphHelper)
+
+	var choice int64 = -1
+
+	for {
+		fmt.Println("Please choose one of the following options:")
+		fmt.Println("0. Exit")
+		fmt.Println("1. Display access token")
+		fmt.Println("2. List my inbox")
+		fmt.Println("3. Send mail")
+		fmt.Println("4. Make a Graph call")
+
+		_, err = fmt.Scanf("%d", &choice)
+		if err != nil {
+			choice = -1
+		}
+
+		switch choice {
+		case 0:
+			// Exit the program
+			fmt.Println("Goodbye...")
+		case 1:
+			// Display access token
+			displayAccessToken(graphHelper)
+		case 2:
+			// List emails from user's inbox
+			listInbox(graphHelper)
+		case 3:
+			// Send an email message
+			sendMail(graphHelper)
+		case 4:
+			// Run any Graph code
+			makeGraphCall(graphHelper)
+		default:
+			fmt.Println("Invalid choice! Please try again.")
+		}
+
+		if choice == 0 {
+			break
+		}
+	}
+}
+
+func initializeGraph(graphHelper *graphhelper.GraphHelper) {
+	err := graphHelper.InitializeGraphForUserAuth()
+	if err != nil {
+		log.Panicf("Error initializing Graph for user auth: %v\n", err)
+	}
+}
+
+func greetUser(graphHelper *graphhelper.GraphHelper) {
+	// TODO
+}
+
+func displayAccessToken(graphHelper *graphhelper.GraphHelper) {
+	// TODO
+}
+
+func listInbox(graphHelper *graphhelper.GraphHelper) {
+	// TODO
+}
+
+func sendMail(graphHelper *graphhelper.GraphHelper) {
+	// TODO
+}
+
+func makeGraphCall(graphHelper *graphhelper.GraphHelper) {
+	// TODO
+}
