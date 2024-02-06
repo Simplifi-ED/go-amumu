00a1f9db26500993bc80e425fde99087e86ffd3b
Marouane Bouaricha <marouane@itchallenge.fr>
2024-02-06 16:46:28 +0100

    Added forwarding to graph api

--- smtp/server.go
+++ smtp/server.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
+	"go-send/graphhelper"
 	"log"
 	"net"
 	"net/mail"
@@ -25,11 +26,32 @@ func (s *SMTPSERVER) Init(port string) {
 }
 
 func mailHandler(origin net.Addr, from string, to []string, data []byte) error {
+	graphHelper := graphhelper.NewGraphHelper()
+
+	initializeGraph(graphHelper)
+
 	msg, _ := mail.ReadMessage(bytes.NewReader(data))
 	subject := msg.Header.Get("Subject")
 	log.Printf("Received mail from %s for %s with subject %s", from, to[0], subject)
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(msg.Body)
 	log.Printf("Message: %s", buf.String())
+	sendMail(graphHelper, from, to[0], subject, buf.String(), false)
 	return nil
+}
+
+func initializeGraph(graphHelper *graphhelper.GraphHelper) {
+	err := graphHelper.InitializeGraphForAppAuth()
+	if err != nil {
+		log.Panicf("Error initializing Graph for app auth: %v\n", err)
+	}
+}
+
+func sendMail(graphHelper *graphhelper.GraphHelper, sender string, receiver string, subject string, body string, channel bool) {
+	err := graphHelper.SendMail(&subject, &body, sender, &receiver, channel)
+	if err != nil {
+		log.Panicf("Error sending message: %v", err)
+	}
+	fmt.Println("Mail sent.")
+	fmt.Println()
 }
