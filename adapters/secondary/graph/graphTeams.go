2aa0303d5463fb87636d8864b3ce1118f819efb4
Marouane Bouaricha <marouane@itchallenge.fr>
2024-02-21 14:23:10 +0100

    Update teams channel url

--- adapters/secondary/graph/graphTeams.go
+++ adapters/secondary/graph/graphTeams.go
@@ -13,9 +13,9 @@ type GraphTeams struct{}
 func (g *GraphTeams) SendAlert(data entities.WebhookData) error {
 	// setup webhook url
 	organization := "simplifiedfr"
-	gid := "31d891a8-180c-40d2-ba10-002ba8856053"
+	gid := "98956b41-a03b-4cd6-b7b3-13ae45b4c54d"
 	tenantId := "cd8312b6-130d-4078-964d-2faddd3a0aca"
-	altId := "cb84ff9b05404a22a22f8adb36900ecb"
+	altId := "d8fca1942fff424fb56e61656d12b120"
 	gOwner := "7cb5c351-1a58-47ee-b6d2-cb8b652b55c5"
 	webhookUrl := fmt.Sprintf("https://%s.webhook.office.com/webhookb2/%s@%s/IncomingWebhook/%s/%s", organization, gid, tenantId, altId, gOwner)
 
